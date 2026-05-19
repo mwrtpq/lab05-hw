5e28b1d5639c2f6791ba865b57ab88e56d1c5ac2
matvej <.>
2026-05-19 17:22:01 +0300

    added tests and coverage

--- tests/account_test.cpp
+++ tests/account_test.cpp
@@ -4,14 +4,41 @@
 TEST(Account, Constructor)
 {
     Account account(1, 100);
+
+    EXPECT_EQ(account.id(), 1);
     EXPECT_EQ(account.GetBalance(), 100);
+}
+
+TEST(Account, LockUnlock)
+{
+    Account account(1, 100);
+
+    account.Lock();
+    account.Unlock();
+
+    SUCCEED();
 }
 
 TEST(Account, ChangeBalance)
 {
     Account account(1, 100);
+
     account.Lock();
     account.ChangeBalance(50);
+
     EXPECT_EQ(account.GetBalance(), 150);
+
     account.Unlock();
+}
+
+TEST(Account, ChangeBalanceWithoutLock)
+{
+    Account account(1, 100);
+
+    EXPECT_ANY_THROW(account.ChangeBalance(50));
+}
+
+TEST(Account, NegativeBalance)
+{
+    EXPECT_ANY_THROW(Account(1, -100));
 }
--- tests/transaction_test.cpp
+++ tests/transaction_test.cpp
@@ -14,3 +14,97 @@ TEST(Transaction, Transfer)
     EXPECT_EQ(from.GetBalance(), 890);
     EXPECT_EQ(to.GetBalance(), 600);
 }
+#include <gtest/gtest.h>
+
+#include "Account.h"
+#include "Transaction.h"
+
+class MockTransaction : public Transaction
+{
+public:
+    void SaveToDataBase(Account&, Account&, int) override
+    {
+        saved = true;
+    }
+
+    bool saved = false;
+};
+
+TEST(Transaction, SetFee)
+{
+    MockTransaction transaction;
+
+    transaction.set_fee(10);
+
+    EXPECT_EQ(transaction.fee(), 10);
+}
+
+TEST(Transaction, SuccessfulTransfer)
+{
+    Account from(1, 1000);
+    Account to(2, 500);
+
+    MockTransaction transaction;
+
+    transaction.set_fee(10);
+
+    EXPECT_TRUE(transaction.Make(from, to, 100));
+
+    EXPECT_EQ(from.GetBalance(), 890);
+    EXPECT_EQ(to.GetBalance(), 600);
+
+    EXPECT_TRUE(transaction.saved);
+}
+
+TEST(Transaction, SameAccount)
+{
+    Account account(1, 1000);
+
+    MockTransaction transaction;
+
+    EXPECT_THROW(
+        transaction.Make(account, account, 100),
+        std::logic_error
+    );
+}
+
+TEST(Transaction, NegativeSum)
+{
+    Account from(1, 1000);
+    Account to(2, 500);
+
+    MockTransaction transaction;
+
+    EXPECT_THROW(
+        transaction.Make(from, to, -100),
+        std::logic_error
+    );
+}
+
+TEST(Transaction, TooSmallSum)
+{
+    Account from(1, 1000);
+    Account to(2, 500);
+
+    MockTransaction transaction;
+
+    EXPECT_THROW(
+        transaction.Make(from, to, 0),
+        std::logic_error
+    );
+}
+
+TEST(Transaction, NotEnoughMoney)
+{
+    Account from(1, 50);
+    Account to(2, 500);
+
+    MockTransaction transaction;
+
+    transaction.set_fee(10);
+
+    EXPECT_FALSE(transaction.Make(from, to, 100));
+
+    EXPECT_EQ(from.GetBalance(), 50);
+    EXPECT_EQ(to.GetBalance(), 500);
+}
